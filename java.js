b96029cb37cabc443bcc5ffa3c5efafbf6971559
Jordi0M <martinezmat.j@gmail.com>
2020-03-18 21:35:24 +0100

    ..

---
+++ java.js
@@ -0,0 +1,7 @@
+var barajar = ["img/black_prohibition.png", "img/black_rotation.png"];
+    var imagen = document.getElementById("cartita");
+
+    function barajarTodas() {
+
+      document.getElementById("cartita").src = "img/black_prohibition.png"
+    }
